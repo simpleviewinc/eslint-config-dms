b4963037788a4cb445b64254d1ff71115ab05449
Owen Allen <owenallenaz@gmail.com>
2022-02-09 16:20:32 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,86 @@
+module.exports = {
+	"parserOptions": {
+		"ecmaVersion": 11,
+		"sourceType": "module",
+		"ecmaFeatures": {
+			"jsx": true
+		}
+	},
+	"overrides": [
+		{
+			"files": [
+				"**/*.ts",
+				"**/*.tsx"
+			],
+			"parser": "@typescript-eslint/parser",
+			"rules": {
+				"no-unused-vars": "off",
+				"@typescript-eslint/no-unused-vars": [
+					2,
+					{
+						"vars": "all",
+						"args": "none",
+						"ignoreRestSiblings": true
+					}
+				],
+				"@typescript-eslint/no-explicit-any": "off"
+			},
+			"extends": [
+				"eslint:recommended",
+				"plugin:react/recommended",
+				"plugin:react/jsx-runtime",
+				"plugin:@typescript-eslint/recommended"
+			],
+			"plugins": [
+				"@typescript-eslint",
+				"react",
+				"react-hooks"
+			]
+		}
+	],
+	"extends": [
+		"eslint:recommended",
+		"plugin:react/recommended"
+	],
+	"rules": {
+		"prefer-const": 2,
+		"no-unused-vars": [
+			2,
+			{
+				"vars": "all",
+				"args": "none",
+				"ignoreRestSiblings": true
+			}
+		],
+		"no-var": 2,
+		"one-var-declaration-per-line": 2,
+		"indent": [
+			"error",
+			"tab",
+			{
+				"flatTernaryExpressions": true
+			}
+		],
+		"space-infix-ops": 2,
+		"keyword-spacing": 2,
+		"space-before-blocks": 2,
+		"key-spacing": 2
+	},
+	"plugins": [
+		"react",
+		"react-hooks"
+	],
+	"env": {
+		"browser": true,
+		"node": true,
+		"es6": true,
+		"mocha": true,
+		"jest": true
+	},
+	"settings": {
+		"react": {
+			"pragma": "React",
+			"version": "detect"
+		}
+	}
+}
