5581517718be2fe867b32fc7dc856554630f59fd
Owen Allen <owenallenaz@gmail.com>
2022-02-21 15:36:22 -0700

    Adds jsx runtime to the base react definition so it's there for both jsx and tsx.

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ module.exports = {
 			],
 			"extends": [
 				"eslint:recommended",
-				"plugin:react/recommended"
+				"plugin:react/recommended",
+				"plugin:react/jsx-runtime"
 			],
 			"plugins": [
 				"react",
