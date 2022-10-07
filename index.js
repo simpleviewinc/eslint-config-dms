27035acbadbd2c5e3e7223a0bac27f15dfe6b7ba
Owen Allen <owenallenaz@gmail.com>
2022-10-07 11:32:43 -0700

    @typescript-eslint/no-var-requires: off because it's just needed sometimes, especially with esm-loading stupidity problems.

--- index.js
+++ index.js
@@ -71,7 +71,8 @@ module.exports = {
 						"ignoreRestSiblings": true
 					}
 				],
-				"@typescript-eslint/no-explicit-any": "off"
+				"@typescript-eslint/no-explicit-any": "off",
+				"@typescript-eslint/no-var-requires": "off"
 			},
 			"extends": [
 				"eslint:recommended",
